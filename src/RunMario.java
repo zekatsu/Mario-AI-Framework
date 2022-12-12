138445e64acbaf5dc7dadc64e6a1e679d474ab2f
zekatsu <katsuyoso@yahoo.co.jp>
2022-12-12 14:07:16 +0900

    play mario with level select

---
+++ src/RunMario.java
@@ -0,0 +1,45 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Scanner;
+
+import engine.core.MarioGame;
+import engine.core.MarioResult;
+
+public class RunMario {
+    public static void printResults(MarioResult result) {
+        System.out.println("****************************************************************");
+        System.out.println("Game Status: " + result.getGameStatus().toString() +
+                " Percentage Completion: " + result.getCompletionPercentage());
+        System.out.println("Lives: " + result.getCurrentLives() + " Coins: " + result.getCurrentCoins() +
+                " Remaining Time: " + (int) Math.ceil(result.getRemainingTime() / 1000f));
+        System.out.println("Mario State: " + result.getMarioMode() +
+                " (Mushrooms: " + result.getNumCollectedMushrooms() + " Fire Flowers: " + result.getNumCollectedFireflower() + ")");
+        System.out.println("Total Kills: " + result.getKillsTotal() + " (Stomps: " + result.getKillsByStomp() +
+                " Fireballs: " + result.getKillsByFire() + " Shells: " + result.getKillsByShell() +
+                " Falls: " + result.getKillsByFall() + ")");
+        System.out.println("Bricks: " + result.getNumDestroyedBricks() + " Jumps: " + result.getNumJumps() +
+                " Max X Jump: " + result.getMaxXJump() + " Max Air Time: " + result.getMaxJumpAirTime());
+        System.out.println("****************************************************************");
+    }
+
+    public static String getLevel(String filepath) {
+        String content = "";
+        try {
+            content = new String(Files.readAllBytes(Paths.get(filepath)));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return content;
+    }
+
+    public static void main(String[] args) {
+        MarioGame game = new MarioGame();
+        while (true) {
+            Scanner scanner = new Scanner(System.in);
+            String level_name = scanner.next();
+            String level = getLevel("./levels/original/lvl-" + level_name + ".txt");
+            printResults(game.runGame(new agents.human.Agent(), level, 300, 0, true, 24));
+        }
+    }
+}
