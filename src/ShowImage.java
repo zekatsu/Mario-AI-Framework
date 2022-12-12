cef5e5055ead9d8e8933ace74defb1a64b971c15
zekatsu <katsuyoso@yahoo.co.jp>
2022-12-12 14:08:27 +0900

    slider size fix

--- src/ShowImage.java
+++ src/ShowImage.java
@@ -6,28 +6,38 @@ import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 public class ShowImage implements ChangeListener {
-    private final ImageComponent imageComponent;
-    private final JSlider slider;
-    private final JFrame frame;
+    private ImageComponent imageComponent;
+    private JSlider slider;
+    private JFrame frame;
 
     ShowImage() {
         this.imageComponent = new ImageComponent(2);
 
-        this.slider = new JSlider(0, 887);
-        this.slider.addChangeListener(this);
+        try {
+            int countImg = (int) Files.list(Paths.get("data/img")).count();
 
-        JPanel panel = new JPanel();
-        panel.setLayout(new BorderLayout());
-        panel.add(this.imageComponent, BorderLayout.CENTER);
-        panel.add(this.slider, BorderLayout.PAGE_END);
+            this.slider = new JSlider(0, countImg - 1);
+            this.slider.addChangeListener(this);
 
-        this.frame = new JFrame("ShowImage");
-        this.frame.getContentPane().add(panel);
-        this.frame.pack();
-        this.frame.setResizable(false);
-        this.frame.setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
+            JPanel panel = new JPanel();
+            panel.setLayout(new BorderLayout());
+            panel.add(this.imageComponent, BorderLayout.CENTER);
+            panel.add(this.slider, BorderLayout.PAGE_END);
+
+            this.frame = new JFrame("ShowImage");
+            this.frame.getContentPane().add(panel);
+            this.frame.pack();
+            this.frame.setResizable(false);
+            this.frame.setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
+            this.frame.setLocation(400, 0);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
     }
 
     public void set(int i) {
@@ -38,7 +48,7 @@ public class ShowImage implements ChangeListener {
     public void stateChanged(ChangeEvent changeEvent) {
         JSlider slider = (JSlider) changeEvent.getSource();
         try {
-            BufferedImage image = ImageIO.read(new File(String.format("data/%d.png", slider.getValue())));
+            BufferedImage image = ImageIO.read(new File(String.format("data/img/%d.png", slider.getValue())));
             this.imageComponent.setImage(image);
         } catch (IOException e) {
             e.printStackTrace();
