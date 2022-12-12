89accb6ae089ccebfb5a64fc218368de66041518
zekatsu <katsuyoso@yahoo.co.jp>
2022-12-12 15:31:54 +0900

    save data dispose mario game window

--- src/RecordGamePlay.java
+++ src/RecordGamePlay.java
@@ -2,9 +2,14 @@ import engine.core.MarioGame;
 import engine.core.MarioResult;
 import engine.helper.GameStatus;
 
-import java.io.IOException;
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.Scanner;
 
 public class RecordGamePlay {
@@ -21,8 +26,36 @@ public class RecordGamePlay {
             String level = getLevel("./levels/original/lvl-" + level_name + ".txt");
             while (true) {
                 MarioResult result = game.runGame(new agents.human.Agent(), level, 300, 0, true, 24);
+                saveData(result);
+                game.dispose();
                 if (result.getGameStatus() == GameStatus.WIN) break;
             }
+        }
+    }
+
+    private void saveData(MarioResult result) {
+        try {
+            SimpleDateFormat fileNameFormat = new SimpleDateFormat("MM-dd HH:mm:ss");
+            String dateString = fileNameFormat.format(Calendar.getInstance().getTime());
+            String dirString = "data/" + dateString;
+            Files.createDirectory(Paths.get(dirString));
+            Files.createDirectory(Paths.get(dirString + "/img"));
+
+            // save game events
+            FileOutputStream f = new FileOutputStream(dirString + "/gameEvents.dat");
+            BufferedOutputStream b = new BufferedOutputStream(f);
+            ObjectOutputStream out = new ObjectOutputStream(b);
+            out.writeObject(result.getGameEvents());
+            out.flush();
+
+            // save screenshots
+            ArrayList<BufferedImage> video = result.getSnapshots();
+            for (int i = 0; i < video.size(); i++) {
+                BufferedImage image = video.get(i);
+                ImageIO.write(image, "png", new File(String.format(dirString + "/img/%d.png", i)));
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 
