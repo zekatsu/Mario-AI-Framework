b2459f90f964b1476cab37d23fc77a0b549571f9
zekatsu <katsuyoso@yahoo.co.jp>
2022-12-29 11:57:24 +0900

    unique

--- src/scripts/analyze.py
+++ src/scripts/analyze.py
@@ -1,11 +1,9 @@
+# %%
 import numpy as np
 import pandas as pd
-
 # %%
-df = pd.read_csv("gameEvents.csv", header=None)
+df = pd.read_csv("../../data/gameEvents.csv", header=None)
 df.head()
 # %%
-for i in range(df.shape[0] - 1):
-    if df.iat[i, 0] > df.iat[i + 1, 0]:
-        print(df.iloc[i])
+df[2].unique()
 # %%
